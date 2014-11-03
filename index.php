f43b4583138858f130a21557fbead4fddca9a9b9
greenua14 <greenua14@gmail.com>
2014-11-03 20:16:17 +0200

    add phpunit and some requets-response code

--- index.php
+++ index.php
@@ -28,3 +28,25 @@ $dispatcher = new Dispatcher($router);
 $response = $dispatcher->dispatch($request->getMethod(), parse_url($request->getPathInfo(), PHP_URL_PATH));
 
 $response->send();
+
+echo '<hr /> <h2>Request</h2>';
+
+$uri = $_SERVER['REQUEST_URI'];
+
+echo 'The URI requested is: ' . $uri . '<br />';
+echo 'Path info: ' . $request->getPathInfo() . '<br />';
+echo 'Server info: ' . $request->server->get('HTTP_HOST') . '<br />';
+echo 'SessID info: ' . $request->cookies->get('PHPSESSID') . '<br />';
+echo 'Headers host info: ' . $request->headers->get('host') . '<br />';
+echo 'Headers content-type info: ' . $request->headers->get('content_type') . '<br />';
+echo 'Request method info: ' . $request->getMethod() .'<br />';
+
+echo '<hr /> <h2>Response</h2>';
+
+$testResponse = new Response();
+
+$testResponse->setContent('<p>Test Response content</p>');
+$testResponse->setStatusCode(Response::HTTP_OK);
+$testResponse->headers->set('Content-Type', 'text/html');
+
+$testResponse->send();
